e0a84c439f0e1091daba85b7337461b3260729bd
kon2raya24 <konturaya@gmail.com>
2026-06-27 09:02:10 +0000

    docs: add JSDoc comments to sleep.ts

--- src/sleep.ts
+++ src/sleep.ts
@@ -1,3 +1,7 @@
+/**
+ * sleep
+ * @param ms
+ */
 export function sleep(ms: number): Promise<void> {
   if (ms < 0) throw new Error("Delay must be non-negative");
   return new Promise(resolve => setTimeout(resolve, ms));
